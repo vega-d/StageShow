2b9f93979019775b02a3b4e9ede94f0ad0bc785d
vega-d <vegad0100110@gmail.com>
2021-07-19 12:29:11 +0600

    Significant performance improvements!

--- face_tracker.py
+++ face_tracker.py
@@ -23,11 +23,11 @@ def argsParser():
                     help="How close to center of frame face should be? 0.1-0.9, Bigger number is closer to center. Default is 0.8")
     ap.add_argument("-a", "--anonymous", type=bool, default=0,
                     help="enable blanking out detected faces to avoid flashing your face.")
-    ap.add_argument("-vh", "--visor_height", type=int, default=500,
+    ap.add_argument("-vh", "--visor_height", type=int, default=150,
                     help="Height in pixels for resolution in which webcam will be processed. Default is 500")
-    ap.add_argument("-oh", "--output_height", type=int, default=360,
+    ap.add_argument("-oh", "--output_height", type=int, default=480,
                     help="Height in pixels for resolution which will be used for output. Default is 360")
-    ap.add_argument("-ow", "--output_width", type=int, default=640,
+    ap.add_argument("-ow", "--output_width", type=int, default=854,
                     help="Width in pixels for resolution which will be used for output. Default is 640")
     ap.add_argument("--debug", help="Enable debug mode.", action="store_true", default=False)
 
@@ -68,31 +68,37 @@ def run_face_tracker(args):
     anon = args["anonymous"]
     debug = args["debug"]
     resize = args["visor_height"]
-    imagemagicsize = [args["output_width"], args["output_height"]]
+    process2render_coefficient = resize / height
+    renderframe_size = [args["output_width"], args["output_height"]]
+    imagemagicsize = [int(i * process2render_coefficient) for i in renderframe_size]
     tolerance = float(args["precision"] / 2)
-    cam = pyvirtualcam.Camera(width=imagemagicsize[0], height=imagemagicsize[1], fps=60)
+    cam = pyvirtualcam.Camera(width=renderframe_size[0], height=renderframe_size[1], fps=60)
     print("[INFO] Output virtual webcam is:", cam.device)
-    print("[INFO] To test view the output execute: ffplay" + cam.device)
+    print("[INFO] To test view the output execute: ffplay", cam.device)
     print("[INFO] Anonymous mode is", "on" if anon else "off")
     print("[INFO] Debug mode is", "on" if debug else "off")
+    print("[INFO] process to render quality coefficient is", process2render_coefficient)
 
-    screenboxX, screenboxY = width // 2, height // 2
+    screenboxX, screenboxY = width // 2 - imagemagicsize[0] // 2, height // 2 - imagemagicsize[1] // 2
 
     # loop over the frames from the video stream
     while True:
         # read the next frame from the video stream and resize it
-        frame = vs.read()[1]
-        frame = imutils.resize(frame, height=resize)
-        debug_frame = frame[:]
+        detect_frame = vs.read()[1]
+        render_frame = detect_frame[:]
+        detect_frame = imutils.resize(detect_frame, height=resize)
+        if debug:
+            debug_frame = detect_frame[:]
+
 
         # if the frame dimensions are None, grab them
         if W is None or H is None:
-            (H, W) = frame.shape[:2]
+            (H, W) = detect_frame.shape[:2]
 
         # construct a blob from the frame, pass it through the network,
         # obtain our output predictions, and initialize the list of
         # bounding box rectangles
-        blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H), (104.0, 177.0, 123.0))
+        blob = cv2.dnn.blobFromImage(detect_frame, 1.0, (W, H), (104.0, 177.0, 123.0))
         net.setInput(blob)
         detections = net.forward()
         rects = []
@@ -114,7 +120,7 @@ def run_face_tracker(args):
                 if anon:
                     points = np.array([[startX - 20, startY - 20], [endX + 20, startY - 20], [endX + 20, endY + 20],
                                        [startX - 20, endY + 20]], dtype=np.int32)
-                    cv2.fillPoly(frame, [points], (0, 0, 0))
+                    cv2.fillPoly(detect_frame, [points], (0, 0, 0))
 
         # update our centroid tracker using the computed set of bounding
         # box rectangles
@@ -126,7 +132,7 @@ def run_face_tracker(args):
             objectID, centroid = tracking_object
 
             if anon:
-                cv2.putText(frame, "anonymous mode on", (centroid[0] - 85, centroid[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                cv2.putText(detect_frame, "anonymous mode on", (centroid[0] - 85, centroid[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (255, 255, 255), 2)
 
             speed = int(distance_from(centroid,
@@ -146,40 +152,43 @@ def run_face_tracker(args):
             # print(speed, screenboxX, screenboxY)
 
             if debug:
-                cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
+                cv2.rectangle(detect_frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
 
                 text1 = str(imagemagicsize)
-                cv2.putText(frame, text1, (screenboxX + 20, screenboxY + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                cv2.putText(detect_frame, text1, (screenboxX + 20, screenboxY + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (255, 255, 255), 2)
 
-                cv2.rectangle(frame, (
+                cv2.rectangle(detect_frame, (
                     screenboxX + int(imagemagicsize[0] * tolerance), int(screenboxY + imagemagicsize[1] * tolerance)),
                               (screenboxX + int(imagemagicsize[0] * (1 - tolerance)),
                                screenboxY + int(imagemagicsize[1] * (1 - tolerance))), (255, 0, 255), 2)
 
                 text = ("occ: " + str(debug))
-                cv2.putText(frame, text, (screenboxX + 20, screenboxY + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                cv2.putText(detect_frame, text, (screenboxX + 20, screenboxY + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (255, 255, 255), 2)
 
             screenboxX, screenboxY = min(max(screenboxX, 0), W - imagemagicsize[0]), min(max(screenboxY, 0),
                                                                                          H - imagemagicsize[1])
 
             if debug:
-                debug_frame = frame[:]
+                # debug_frame = detect_frame[:]
                 cv2.rectangle(debug_frame, (screenboxX, screenboxY),
                               (screenboxX + imagemagicsize[0], screenboxY + imagemagicsize[1]), (0, 0, 255), 2)
         else:
             # frame = frame[0:imagemagicsize[1], 0:imagemagicsize[0]]
             text = "No face found !"
-            cv2.putText(frame, text, (screenboxX + 20, screenboxY + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),
-                        2)
-        frame = frame[screenboxY:screenboxY + imagemagicsize[1], screenboxX:screenboxX + imagemagicsize[0]]
+            cv2.putText(detect_frame, text, (screenboxX + 20, screenboxY + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        renderX, renderY = int(screenboxX / process2render_coefficient), int(screenboxY / process2render_coefficient)
+
+        render_frame = render_frame[renderY:renderY + renderframe_size[1], renderX:renderX + renderframe_size[0]]
         if debug:
             cv2.imshow(cam.device, debug_frame)
 
-        frame = imutils.resize(frame, width=args["output_width"], height=args["output_height"])
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        cam.send(frame)
+        # render_frame = render_frame[0:renderframe_size[1], 0:renderframe_size[0]]
+        render_frame = imutils.resize(render_frame, height=args["output_height"], width=args["output_width"])
+        render_frame = cv2.cvtColor(render_frame, cv2.COLOR_RGB2BGR)
+        cam.send(render_frame)
 
         cam.sleep_until_next_frame()
         key = cv2.waitKey(1) & 0xFF
--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
-sudo modprobe v4l2loopback card_label="Stage Camera" video_nr=2 exclusive_caps=1
+
 
 if [ $1 == "--setup" ]; then
-  echo "Installing dependencies!"
+  echo "[INFO] Installing dependencies for StageShow!"
   pip3 install numpy opencv_python pyvirtualcam argparse scipy imutils
-  sudo apt install v4l2loopback-dkms
+  sudo apt install v4l2loopback-dkms v4l2loopback-utils
+  echo "[Warning] You might have to reboot now!"
 else
+  echo "[INFO] Loading in virtual webcam drivers..."
+  sudo modprobe -r v4l2loopback
+  sudo modprobe v4l2loopback card_label="Dummy Camera" video_nr=2 exclusive_caps=1
+  echo "[INFO] Loaded in virtual webcam drivers!"
   python3 face_tracker.py $@
 fi
 # Copyright https://github/vega-d 2021
